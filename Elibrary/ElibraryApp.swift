117e6998ccfd2409636f1509914651095106966e
Pavel Odstrčilík <pavel.odstrcilik@b2a.cz>
2022-10-22 11:22:58 +0200

    [Feature] Init

---
+++ Elibrary/ElibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElibraryApp.swift
+//  Elibrary
+//
+//  Created by Pavel Odstrčilík on 22.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ElibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
